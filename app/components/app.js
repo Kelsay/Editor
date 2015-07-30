05ab8f03d4719c1136e129af2c812cc8464a0e14
Kelsay <fijolek.krzysztof@gmail.com>
2015-07-30 13:01:15 +0100

    App update

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,12 @@
-'use strict'; 
+'use strict';
 // The Main Application module
 
-var app = angular.module('ClauseMatch', ['ngAnimate', 'ui.router','contenteditable','dndLists'])
+var app = angular.module('ClauseMatch', ['ngAnimate', 'ui.router', 'contenteditable', 'dndLists'])
 
 // Default UI State
 .config(['$stateProvider', function ($stateProvider) {
-    $stateProvider.state("about", {
+    $stateProvider.state("home", {
         url: "",
         templateUrl: "build/templates/home.html"
-    })
-}])
+    });
+}]);
